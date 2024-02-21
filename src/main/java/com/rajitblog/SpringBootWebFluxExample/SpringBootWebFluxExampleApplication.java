c4ddf9bc61249911b59411ea168bbac19e36de12
Rajesh Kumar <kumarrajesh7671@gmail.com>
2024-02-21 21:07:40 +0530

    initial commit

---
+++ src/main/java/com/rajitblog/SpringBootWebFluxExample/SpringBootWebFluxExampleApplication.java
@@ -0,0 +1,13 @@
+package com.rajitblog.SpringBootWebFluxExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebFluxExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebFluxExampleApplication.class, args);
+	}
+
+}
